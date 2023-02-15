4d0281697246ebf30b8e917a97ef0663da612d84
ccalderong94 <carloscalderon@chicagobooth.edu>
2023-02-15 20:39:44 +0000

    1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     matching_users = User.all
     @users = matching_users.order(:created_at)
 
-    render({ :template => "users_templates/all_users.html.erb"})
+    render({ :template => "user_templates/all_users.html.erb"})
   end
   
   def show
@@ -16,21 +16,30 @@ class UsersController < ApplicationController
   end
   
   def create
+    query_username = params.fetch("query_username")
+    
     user = User.new
-    user.username = params.fetch("query_username")
+    user.username = query_username
     user.save
     
     redirect_to("/users/#{user.username}")
   end
   
+
+
+
   def update
-    user_id = params.fetch("user_id")
+    
+    user_id = params.fetch("modify_user")
     matching_users = User.where({ :id => user_id })
     the_user = matching_users.at(0)
     
-    the_user.username = params.fetch("query_username")
+    new_username = params.fetch("query_username")
+
+    the_user.username = new_username
     the_user.save
-    redirect_to("/users/#{user.username}")
+    
+    redirect_to("/users/"+the_user.username.to_s)
   end
 
 end
